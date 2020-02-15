aeedbc7b2ebd28413a8b1780fc75bda8c166415b
rkodang <245894317@qq.com>
2020-02-15 18:53:21 +0800

    Static<E>

--- src/main/java/com/gundom/Generic/TextArrayList.java
+++ src/main/java/com/gundom/Generic/TextArrayList.java
@@ -4,7 +4,8 @@ package com.gundom.Generic;
 import java.util.ArrayList;
 
 class LockArrayList<E> extends ArrayList<E> {
-    
+
+    public static <E>void sumSize(E e){}
     @Override
     public synchronized boolean add(E e) {
         return super.add(e);
