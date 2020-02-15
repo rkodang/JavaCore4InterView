46a76fd983f30d1c4087089c54e38500dd52403d
rkodang <245894317@qq.com>
2020-02-15 13:38:59 +0800

    基于Mybaits的LruCache仿写一段LruCache

---
+++ src/main/java/com/gundom/Cache/LruCache.java
@@ -0,0 +1,73 @@
+package com.gundom.Cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LruCache implements Cache {
+
+    private final Cache cache;
+    private Map<Object,Object> keymap;
+    private Object eldestKey;
+    public LruCache(Cache cache) {
+        this.cache=cache;
+        setSize(3);
+    }
+
+    private void setSize(final int size) {
+        keymap=new LinkedHashMap<Object, Object>(size,0.75f,true){
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Object, Object> eldest) {
+                boolean tooBig=size()>size;
+                if (tooBig) {
+                    eldestKey=eldest.getKey();
+                }
+
+                return tooBig;
+            }
+        };
+    }
+
+    @Override
+    public void putObejct(Object key, Object value) {
+        cache.putObejct(key, value);
+        removeOldObject(key);
+    }
+
+    private void removeOldObject(Object key) {
+        keymap.put(key,key);
+        if (eldestKey != null) {
+            cache.removeObject(eldestKey);
+            eldestKey=null;
+        }
+    }
+
+    @Override
+    public Object getObject(Object key) {
+        keymap.get(key);
+        return cache.getObject(key);
+    }
+
+    @Override
+    public Object removeObject(Object key) {
+        return cache.removeObject(key);
+    }
+
+
+    @Override
+    public String toString() {
+        return "LruCache{" +
+                "cache=" + cache +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        LruCache cache=new LruCache(new PerpetualCache());
+        cache.putObejct("A",100);
+        cache.putObejct("B",300);
+        cache.putObejct("C",500);
+        cache.getObject("A");
+        cache.putObejct("P",800);
+        System.out.println(cache);
+    }
+
+}
