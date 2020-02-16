7db4ac27094aaaaf3f19ca7dca9561bb9f71eaf0
rkodang <245894317@qq.com>
2020-02-16 14:11:35 +0800

    .

--- Text-MyBatis/src/main/java/com/gumdom/TestBase.java
+++ Text-MyBatis/src/main/java/com/gumdom/TestBase.java
@@ -1,4 +1,35 @@
 package com.gumdom;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+
 public class TestBase {
+    private SqlSessionFactory build;
+
+    @Before
+    public void init() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-configs.xml");
+         this.build = new SqlSessionFactoryBuilder().build(resourceAsStream);
+    }
+    public static void main(String[] args) {
+        String url="classpath:mybatis-configs.xml";
+    }
+
+    @Test
+    public void TextSqlSessionFactory(){
+        System.out.println(build);
+        SqlSession sqlSession = build.openSession();
+        Connection connection = sqlSession.getConnection();
+        System.out.println(connection);
+
+    }
+
 }
