7feaa4f5a0ce658c4bbc1b6369b7b1d344aedf5a
rkodang <245894317@qq.com>
2020-02-16 10:34:12 +0800

    Kryo序列化

---
+++ src/main/java/com/gundom/Serializable/HelloKryo.java
@@ -0,0 +1,44 @@
+package com.gundom.Serializable;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class HelloKryo {
+
+    public static void main(String[] args) throws Exception{
+        Kryo kryo = new Kryo();
+        kryo.register(Message.class);
+
+        Message object = new Message();
+        object.value = "Hello Kryo!";
+        object.i=5;
+
+        Output output = new Output(new FileOutputStream("file.bin"));
+        kryo.writeObject(output, object);
+        output.close();
+
+        System.out.println(output);
+
+        Input input = new Input(new FileInputStream("file.bin"));
+        System.out.println(input);
+        Message object2 = kryo.readObject(input, Message.class);
+        input.close();
+        System.out.println(object2);
+    }
+    static public class Message{
+        String value;
+        private int i;
+
+        @Override
+        public String toString() {
+            return "Message{" +
+                    "value='" + value + '\'' +
+                    ", i=" + i +
+                    '}';
+        }
+    }
+}
