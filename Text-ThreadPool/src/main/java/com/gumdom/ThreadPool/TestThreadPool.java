0a8972ad484adb07e9670aa131736896620a9d1c
rkodang <245894317@qq.com>
2020-02-21 20:26:17 +0800

    ThreadPool

--- Text-ThreadPool/src/main/java/com/gumdom/ThreadPool/TestThreadPool.java
+++ Text-ThreadPool/src/main/java/com/gumdom/ThreadPool/TestThreadPool.java
@@ -1,4 +1,27 @@
 package com.gumdom.ThreadPool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class TestThreadPool {
+
+
+    public static void main(String[] args) {
+        ExecutorService threadpool= Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+
+            threadpool.submit(()->{
+                System.out.println("currentName"+Thread.currentThread().getName());
+
+                try {
+                    Object object =null;
+                    System.out.println("result**"+object.toString());
+                } catch (Exception e) {
+                    System.out.println("异常来了"+e);
+                }
+
+            });
+
+        }
+    }
 }
