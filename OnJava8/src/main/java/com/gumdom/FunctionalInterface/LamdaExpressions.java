1e84e190f4bc99da19e44999f2066dd6635b4ef4
rkodang <245894317@qq.com>
2020-03-12 12:32:55 +0800

    OnJava8.FunctionalInterface.Lambda01

--- OnJava8/src/main/java/com/gumdom/FunctionalInterface/LamdaExpressions.java
+++ OnJava8/src/main/java/com/gumdom/FunctionalInterface/LamdaExpressions.java
@@ -16,15 +16,17 @@ interface Multi {
 
 public class LamdaExpressions {
 
-    static Body bod = h -> h + " No Parens!"; // [1]
+    static Body bod = h -> h + " No Parens!"; // [1]h是代表head形参参数,只有一个参数可以不写括号
 
-    static Body bod2 = (h) -> h + " More details"; // [2]
+    // [2]->正常情况适用()包裹参数,为了保持一致性,也可以使用()包裹单个参数
+    static Body bod2 = (h) -> h + " More details";
 
-    static Description desc = () -> "Short info"; // [3]
+    static Description desc = () -> "Short info"; // [3]->没有参数必须用()表示空参数列表
 
-    static Multi mult = (h, n) -> h + n; // [4]
+    static Multi mult = (h, n) -> h + n; // [4]多个参数则必须使用()来表示空参数列表
 
-    static Description moreLines = () -> { // [5]
+    // [5]如果Lambda表达式中确实需要多行就必须将代码写在{}内
+    static Description moreLines = () -> {
         System.out.println("moreLines()");
         return "from moreLines()";
     };
